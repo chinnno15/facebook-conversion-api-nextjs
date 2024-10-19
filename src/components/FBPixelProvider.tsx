caada5a5d78e8491fbcd28e797a84b1fdf3818c6
Jose Oliveros <chinnno15@gmail.com>
2024-10-19 15:10:35 -0700

    update to work with nextjs 15
    Signed-off-by: Jose Oliveros <chinnno15@gmail.com>

--- src/components/FBPixelProvider.tsx
+++ src/components/FBPixelProvider.tsx
@@ -1,29 +1,32 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { fbPageView } from '../conversion-api';
+'use client';
+
+import React, {useEffect} from 'react';
+// import {useRouter} from 'next/router';
+import {fbPageView} from '../conversion-api';
 
 type Props = {
-  children: React.ReactNode
+    children: React.ReactNode
 };
 
-const FBPixelProvider = ({ children }: Props) => {
-  const router = useRouter();
+const FBPixelProvider = ({children}: Props) => {
+    console.log('###### FBPixelProvider #####');
+    // const router = useRouter();
 
-  useEffect(() => {
-    fbPageView();
+    useEffect(() => {
+        fbPageView();
 
-    router.events.on('routeChangeComplete', fbPageView);
-    return () => {
-      router.events.off('routeChangeComplete', fbPageView);
-    };
-  }, [router.events]);
+        // router.events.on('routeChangeComplete', fbPageView);
+        // return () => {
+        //     router.events.off('routeChangeComplete', fbPageView);
+        // };
+    }, []);
 
-  return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-      {children}
-    </>
-  );
+    return (
+        // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            {children}
+        </>
+    );
 };
 
 export default FBPixelProvider;
